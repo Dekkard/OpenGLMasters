1db9eff03bfca446dee8c2921b6dc43ef27e4188
Gustavo Rocha <gustavo.rrff@gmail.com>
2018-10-13 18:16:52 -0300

    Adicionando JOGL ao projeto

---
+++ src/br/com/CG/EventListener.java
@@ -0,0 +1,27 @@
+package br.com.CG;
+
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLEventListener;
+
+public class EventListener implements GLEventListener{
+
+	public void init(GLAutoDrawable drawable) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void dispose(GLAutoDrawable drawable) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void display(GLAutoDrawable drawable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
