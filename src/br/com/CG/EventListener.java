4bbf60ab8b9cdd63bb3e4c4bca4cf5e40358c611
alunotgn <alunotgn@SB116-26>
2018-10-17 11:10:05 -0300

    Adicionar evento de mouse

--- src/br/com/CG/EventListener.java
+++ src/br/com/CG/EventListener.java
@@ -1,27 +1,51 @@
 package br.com.CG;
 
+import javax.swing.*;
 import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLCapabilities;
 import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.GLProfile;
+import com.jogamp.opengl.awt.GLCanvas;
 
-public class EventListener implements GLEventListener{
+public class EventListener implements GLEventListener {
 
 	public void init(GLAutoDrawable drawable) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	public void dispose(GLAutoDrawable drawable) {
 		// TODO Auto-generated method stub
 
 	}
-	
+
 	public void display(GLAutoDrawable drawable) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	public static void main(String[] args) {
+		// getting the capabilities object of GL2 profile
+		final GLProfile profile = GLProfile.get(GLProfile.GL2);
+		GLCapabilities capabilities = new GLCapabilities(profile);
+
+		// The canvas
+		final GLCanvas glcanvas = new GLCanvas(capabilities);
+		EventListener b = new EventListener();
+		glcanvas.addGLEventListener(b);
+		glcanvas.setSize(400, 400);
+
+		// creating frame
+		final JFrame frame = new JFrame("Basic Frame");
+
+		// adding canvas to frame
+		frame.add(glcanvas);
+		frame.setSize(640,480);
+		frame.setVisible(true);
+	}
 }
---
+++ src/br/com/CG/MouseListener.java
@@ -0,0 +1,47 @@
+package br.com.CG;
+
+import com.jogamp.newt.event.MouseEvent;
+
+public class MouseListener implements com.jogamp.newt.event.MouseListener{
+
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseWheelMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
