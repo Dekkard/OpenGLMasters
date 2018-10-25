5ec113fbe45398c6b668cfa278ca766261b4a3e1
Dekkard <gustavo.rrff@gmail.com>
2018-10-25 11:25:47 -0300

    Adicionando Swing design

--- src/br/com/CG/Frame.java
+++ src/br/com/CG/Frame.java
@@ -1,17 +1,19 @@
 package br.com.CG;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+
+import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLCapabilities;
 import com.jogamp.opengl.GLEventListener;
 import com.jogamp.opengl.GLProfile;
 import com.jogamp.opengl.awt.GLCanvas;
 
-public class EventListener implements GLEventListener {
+public class Frame implements GLEventListener {
 
 	public void init(GLAutoDrawable drawable) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	public void dispose(GLAutoDrawable drawable) {
@@ -21,7 +23,14 @@ public class EventListener implements GLEventListener {
 
 	public void display(GLAutoDrawable drawable) {
 		// TODO Auto-generated method stub
-
+		final GL2 gl = drawable.getGL().getGL2();
+		
+		gl.glBegin(GL2.GL_POLYGON );// static field
+		gl.glColor3f(1.0f, 0.0f, 0.0f);
+		gl.glVertex3f(0.50f, -0.50f, 0);
+		gl.glVertex3f(-0.50f, 0.50f, 0);
+		gl.glVertex3f(-0.50f, -0.50f, 0);
+		gl.glEnd();
 	}
 
 	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
@@ -36,7 +45,7 @@ public class EventListener implements GLEventListener {
 
 		// The canvas
 		final GLCanvas glcanvas = new GLCanvas(capabilities);
-		EventListener b = new EventListener();
+		Frame b = new Frame();
 		glcanvas.addGLEventListener(b);
 		glcanvas.setSize(400, 400);
 
@@ -44,8 +53,28 @@ public class EventListener implements GLEventListener {
 		final JFrame frame = new JFrame("Basic Frame");
 
 		// adding canvas to frame
-		frame.add(glcanvas);
-		frame.setSize(640,480);
+		frame.getContentPane().add(glcanvas);
+		frame.setSize(frame.getContentPane().getPreferredSize());
 		frame.setVisible(true);
+	}
+	
+	public static GLCanvas drawCanvas() {
+		final GLProfile profile = GLProfile.get(GLProfile.GL2);
+		GLCapabilities capabilities = new GLCapabilities(profile);
+
+		// The canvas
+		final GLCanvas glcanvas = new GLCanvas(capabilities);
+		Frame b = new Frame();
+		glcanvas.addGLEventListener(b);
+		
+//		glcanvas.setSize(400, 400);
+//		
+//		JPanel panel = new JPanel();
+//		
+//		panel.add(glcanvas);
+//		panel.setSize(panel.getPreferredSize());
+//		panel.setVisible(true);
+		
+		return glcanvas;
 	}
 }
--- src/br/com/GUI/MainInterface.java
+++ src/br/com/GUI/MainInterface.java
@@ -3,6 +3,17 @@ package br.com.GUI;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.CompoundBorder;
+
+import com.jogamp.opengl.awt.GLCanvas;
+
+import br.com.CG.Frame;
 
 public class MainInterface {
 
@@ -36,8 +47,39 @@ public class MainInterface {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.setLocationRelativeTo(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new CompoundBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null), null));
+		panel.setBounds(10, 11, 588, 518);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+//		JPanel glPanel = new JPanel();
+//		glPanel.setBounds(50, 50, 568, 496);
+//		glPanel = Frame.drawFrame();
+//		panel.add(glPanel);
+		
+		GLCanvas glcanvas = new GLCanvas();
+		glcanvas = Frame.drawCanvas();
+		glcanvas.setSize(588, 518);
+		panel.add(glcanvas);
+		
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(608, 11, 166, 518);
+		frame.getContentPane().add(scrollPane);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnArquivo = new JMenu("Arquivo");
+		menuBar.add(mnArquivo);
+		
+		JMenuItem mntmSair = new JMenuItem("Sair");
+		mnArquivo.add(mntmSair);
 	}
-
 }
